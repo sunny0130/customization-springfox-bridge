4bcee58b1ef65a430cec2e134a7779370d0c1ea5
linyang.wly <wly01693207@cainiao.com>
2023-09-10 14:18:52 +0800

    init

---
+++ customization-springfox-bridge-core/src/main/java/io/springfox/bridge/core/model/GenericArrayInfo.java
@@ -0,0 +1,19 @@
+package io.springfox.bridge.core.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenericArrayInfo {
+    /**
+     *
+     * four types:1: string,generic name; 2,Class: concrete class; 3,GenericInfo: GenericInfo object; 4,GenericArrayInfo: GenericArrayInfo object
+     */
+    private Object info;
+}
---
+++ customization-springfox-bridge-core/src/main/java/io/springfox/bridge/core/util/FileUtil.java
@@ -0,0 +1,16 @@
+package io.springfox.bridge.core.util;
+
+import java.io.File;
+
+public class FileUtil {
+    private static String currentFilePath = "";
+
+    public static String getCurrentFilePath() {
+        if (StringUtil.isBlank(currentFilePath)) {
+            File file = new File("");
+            currentFilePath = file.getAbsolutePath();
+        }
+        return currentFilePath;
+    }
+
+}
